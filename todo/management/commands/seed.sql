b3587c66f760920d41d545455711170db87a4ee1
omnrohr <67183475+omnrohr@users.noreply.github.com>
2021-12-28 23:16:00 +0200

    Add seeding data in commands

---
+++ todo/management/commands/seed.sql
@@ -0,0 +1,8 @@
+insert into
+  todo_task (id, title, Completed,added)
+values
+  (1, 'Collecting Flowers', true, '2021-12-27 18:20:58.624501'),
+  (2, 'Go to Grocery', false, '2021-12-27 18:20:58.624522'),
+  (3, 'Go to Beauty salon', false, '2021-12-27 18:20:58.624550'),
+  (4, 'Do Home Cleaning', true, '2021-12-27 18:20:58.624580'),
+  (5, 'Play with kids', false, '2021-12-27 18:20:58.624200');
